d13920643d09dba327831242229bcbaf74fbc579
alrayyan2157 <alrayyan2157@icloud.com>
2026-05-13 08:11:42 +0530

    playRound function added

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ function getHumanChoice() {
     return hchoice;
 }
 
+function playRound(humanChoice, computerChoice, humanScore, computerScore) {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+    if (humanChoice == computerChoice){
+        console.log(`Both chose ${humanChoice}. Draw!`);
+    } else if ((humanChoice == "rock" && computerChoice == "scissors")||(humanChoice == "paper" && computerChoice == "rock")||(humanChoice == "scissors" && computerChoice == "paper")) {
+        console.log(`${humanChoice} beats ${computerChoice}. You win this round!`);
+        humanScore++;
+    } else if ((computerChoice == "rock" && humanChoice == "scissors")||(computerChoice == "paper" && humanChoice == "rock")||(computerChoice == "scissors" && humanChoice == "paper")) {
+        console.log(`${computerChoice} beats ${humanChoice}. Computer win this round!`);
+        computerScore++;
+    }
+}
+
